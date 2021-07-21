3797b1ffe84d1a7174436b7582fe7250b4b07487
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-07-21 22:42:05 +0545

    testing getserversideprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next'
+import { useEffect } from 'react'
 import About from '../components/About'
 import Blog from '../components/Blog'
 import Contact from '../components/Contact'
@@ -6,16 +8,30 @@ import Introduction from '../components/Introduction'
 import Project from '../components/Project'
 import SkillExperience from '../components/SkillExperience'
 
-export default function Home() {
-  return (
-    <>
-        <Introduction/>
-        <About/>
-        <SkillExperience/>
-        <Project/>
-        <Blog/>
-        <Contact/>
-        <Footer/>
-    </>
-  )
+const Home = ({data}: any) => {
+    useEffect(() => {
+        console.log(data);
+    }, [])
+    return (
+        <>
+            <Introduction/>
+            <About/>
+            <SkillExperience/>
+            <Project/>
+            <Blog/>
+            <Contact/>
+            <Footer/>
+        </>
+    )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const host = context.req.headers.host;
+    const protocol = host?.includes('localhost') ? 'http' : 'https';
+    const res = await fetch(`${protocol}://${host}/api/aboutme`);
+    const data = await res.json();
+
+    return { props: { data } }
+}
+
+export default Home;
